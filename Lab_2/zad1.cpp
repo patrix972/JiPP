8671dee64f864fa8a933b23814ad8b4fd14978d1
patrix972 <patrixdudix4@gmail.com>
2020-11-03 02:25:22 +0100

    zad1

---
+++ Lab_2/zad1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void swapValues(int *a, int *b) {
+    if (*a > *b) {
+       int temp = *a;
+       *a = *b;
+       *b = temp;
+    }
+}
+
+int main() {
+    int first_value, second_value;
+
+    cout << "podaj pierwsza zmienna: ";
+    cin >> first_value;
+    cout << "podaj druga zmienna: ";
+    cin >> second_value;
+
+    swapValues(&first_value, &second_value);
+
+    cout << "zmienna a = " << first_value << " zmienna b = " << second_value;
+
+    return 0;
+
+}
